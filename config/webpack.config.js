60564fdc9abb77b49b0616f0ef3c5936fb975a37
chu <chu295@qq.com>
2019-12-02 20:28:09 +0800

    处理html

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -356,7 +356,10 @@ module.exports = function(webpackEnv) {
       rules: [
         // Disable require.ensure as it's not a standard language feature.
         { parser: { requireEnsure: false } },
-
+        {
+          test: /.hbs$/,
+          loader: require.resolve('handlebars-loader')
+        },
         // First, run the linter.
         // It's important to do this before Babel processes the JS.
         {
